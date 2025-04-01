83eb95332a01afd6b21ad350e8cb58077834cf49
DevNap <thdwodh789@gmail.com>
2025-04-01 15:16:12 +0900

    [Feat] Title Scene Init
    타이틀 씬 구현

--- TextRpg_Reference_WhiteIsland/Game.cs
+++ TextRpg_Reference_WhiteIsland/Game.cs
@@ -6,27 +6,55 @@ using System.Threading.Tasks;
 
 namespace TextRpg_Reference_WhiteIsland
 {
-    // Game class는 처음부터 끝까지 사용할 class 이므로 static으로 생성
+    /// Game class는 처음부터 끝까지 사용할 class 이므로 static으로 생성
     public static class Game
     {
-        // 게임에 필요한 정보들 생성
+        /// <summary>
+        /// 게임에 필요한 정보들 생성 
+        /// </summary>
+        /// 게임 오버가 되기 전까지는 게임 계속 실행
+        public static bool gameOver;
 
-        // 게임 시작
+        /// Scene들이 많이 있으므로 Dictionary로 관리
+        private static Dictionary<string, Scene> sceneDic;
+
+        /// <summary>
+        /// Scene을 담을 수 있는 변수 선언
+        /// </summary>
+        private static Scene curScene;
+
+        /// <summary>
+        /// 게임 시작 
+        /// </summary>
         public static void Start()
         {
 
         }
 
-        // 게임 종료
+        /// <summary>
+        /// 게임 종료
+        /// </summary>
         public static void End()
         {
 
         }
 
-        // 게임 동작
+        /// <summary>
+        /// 게임 동작
+        /// </summary>
         public static void Run()
         {
-
+            // 게임오버가 아니라면? 계속 플레이할 수 있게
+            // 현재의 Scene을 계속 플레이하도록 해야 함.
+            while (gameOver == false)
+            {
+                curScene.RenderScene();
+                curScene.ChoiceScene();
+                curScene.InputScene();
+                curScene.ResultScene();
+                curScene.WaitScene();
+                curScene.NextScene();
+            }
         }
     }
 }
---
+++ TextRpg_Reference_WhiteIsland/Scenes/TitleScene.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRpg_Reference_WhiteIsland.Scenes
+{
+    public class TitleScene : Scene
+    {
+        public override void RenderScene()
+        {
+            Console.WriteLine("/////..././/././/////.//...///...////");
+            Console.WriteLine("//             하얀 섬             ..");
+            Console.WriteLine("..           White IsLand          //");
+            Console.WriteLine("/..//..././/././/..///.//...///...///");
+            Console.WriteLine();
+        }
+        public override void ChoiceScene()
+        {
+            Console.WriteLine("1. Start Game");
+            Console.WriteLine("2. Load Game");
+            Console.WriteLine("3. Exit");
+        }
+
+        public override void ResultScene()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WaitScene()
+        {
+            throw new NotImplementedException();
+        }
+        public override void NextScene()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
